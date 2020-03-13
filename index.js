f601268516ae6b4db2c3ee9c99f8936569ebaa29
Renan Pupin <renan.pupin@gmail.com>
2020-03-13 12:03:45 -0300

    remove name restrictions

--- index.js
+++ index.js
@@ -10,34 +10,20 @@ fs.readdir(directoryPath, function (err, files) {
     } else {
         files.forEach(function (file) {
             if (file.endsWith('svg')) {
-                if (!file.includes('outline')) {
-                    // console.log('normal');
-
-                    fs.copyFile('./svg/' + file, './newsvgs/' + file, (err) => {
-                        if (err) {
-                            throw err;
-                        } else {
-                            console.log('copy file', file);
-                        }
-                    });
-                } else {
-                    // console.log('outlined');
-
-                    fs.readFile('./svg/' + file, (err, data) => {
-                        if (err) {
-                            throw err;
-                        } else {
-                            outlineStroke(data).then(outlined => {
-                                fs.writeFile('./newsvgs/' + file, outlined, err => {
-                                    if (err) {
-                                        throw err;
-                                    }
-                                    console.log('convert file', file);
-                                });
+                fs.readFile('./svg/' + file, (err, data) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        outlineStroke(data).then(outlined => {
+                            fs.writeFile('./newsvgs/' + file, outlined, err => {
+                                if (err) {
+                                    throw err;
+                                }
+                                console.log('convert file', file);
                             });
-                        }
-                    })
-                }
+                        });
+                    }
+                })
             }
         });
     }
